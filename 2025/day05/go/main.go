6317b363c8813ad13315a5ec26665bd04bff403e
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2025-12-11 14:31:55 +0500

    feat(2025): add day 05 part 2 solution

--- 2025/day05/go/main.go
+++ 2025/day05/go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -19,9 +20,9 @@ func part1(input string) int {
 	result := 0
 	splittedInput := strings.Split(input, "\n\n")
 	freshIdRanges := strings.Split(splittedInput[0], "\n")
-	ingredientIds := strings.Split(splittedInput[1], "\n")
+	ingredientIds := strings.SplitSeq(splittedInput[1], "\n")
 
-	for _, ingredientId := range ingredientIds {
+	for ingredientId := range ingredientIds {
 		for _, freshIdRange := range freshIdRanges {
 			freshIdRangeArr := strings.Split(freshIdRange, "-")
 			ingredient, err := strconv.Atoi(ingredientId)
@@ -41,8 +42,59 @@ func part1(input string) int {
 }
 
 func part2(input string) int {
-	// TODO: Implement part 2
-	return 0
+	result := 0
+	splittedInput := strings.Split(input, "\n\n")
+	freshIdRanges := strings.Split(splittedInput[0], "\n")
+
+	sort.Slice(freshIdRanges, func(i, j int) bool {
+		firstMin, err := strconv.Atoi(strings.Split(freshIdRanges[i], "-")[0])
+		secondMin, err := strconv.Atoi(strings.Split(freshIdRanges[j], "-")[0])
+		if err != nil {
+			panic(err)
+		}
+		return firstMin < secondMin
+	})
+
+	globalMax := 0
+	for i := range freshIdRanges {
+		current := strings.Split(freshIdRanges[i], "-")
+		currentMin, err := strconv.Atoi(current[0])
+		currentMax, err := strconv.Atoi(current[1])
+		if err != nil {
+			panic(err)
+		}
+
+		if i > 0 {
+			previous := strings.Split(freshIdRanges[i-1], "-")
+			previousMin, err := strconv.Atoi(previous[0])
+			previousMax, err := strconv.Atoi(previous[1])
+			if previousMax > globalMax {
+				globalMax = previousMax
+			}
+			if err != nil {
+				panic(err)
+			}
+			if currentMin == previousMin {
+				if currentMax > globalMax {
+					result += currentMax - globalMax
+				}
+			} else {
+				if currentMin < globalMax {
+					if currentMax > globalMax {
+						result += currentMax - globalMax
+					}
+				} else if currentMin == globalMax {
+					result += currentMax - currentMin
+				} else {
+					result += currentMax - currentMin + 1
+				}
+			}
+		} else {
+			result += currentMax - currentMin + 1
+			globalMax = currentMax
+		}
+	}
+	return result
 }
 
 func main() {
--- 2025/day05/go/main_test.go
+++ 2025/day05/go/main_test.go
@@ -24,7 +24,7 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	result := part2(testInput)
-	expected := 0
+	expected := 14
 	if result != expected {
 		t.Errorf("Part 2: expected %d, got %d", expected, result)
 	}
